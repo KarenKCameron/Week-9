a5dc2cab06de6affba2411f68530d0a99db89b79
KarenKCameron <97938892+KarenKCameron@users.noreply.github.com>
2022-03-23 20:40:32 -0700

    Add files via upload

--- higher-order.js
+++ higher-order.js
@@ -1,7 +1,11 @@
 // This function should execute the callback function the number of times specified.
 // When the function is being executed, the repetition number (i.e. 1 for the first call)
 // should be passed to the callback.
-const repeatFn = (times, callback) => {};
+const repeatFn = (times, callback) => {
+for (let index = 0; index < times; index++) {
+  callback(times)
+  }
+};
 
 // Test repeatFn
 const addButton = num => {
@@ -9,14 +13,27 @@ const addButton = num => {
   button.innerText = `Button ${num}`;
   document.querySelector('body').appendChild(button);
 };
-repeatFn(6, addButton);
+repeatFn(8, addButton);
 
 const toThePower = (num, pow) => {
   let product = 1;
   repeatFn(pow, () => {
-    product += product * num;
+    product = product * num;
   });
   return product;
 };
 
 console.log(toThePower(3, 3));
+
+const createCounter = () => {
+  let count = 0;
+  const getCount = () => count;
+  const increaseCount = () => count++;
+  return {
+  getCount,
+  increaseCount
+  };
+  };
+  const counter = createCounter();
+  counter.increaseCount();
+  console.log(counter.getCount());
